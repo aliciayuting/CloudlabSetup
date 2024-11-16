34edbc60b0ff040e7b007a829b16971c839c7d6d
Alicia <Alicia@node2.setupprofile.cascadefuse-pg0.wisc.cloudlab.us>
2024-11-16 14:13:04 -0600

    script for mellanox_ofed setup

---
+++ mellanox_driver_setup.sh
@@ -0,0 +1,6 @@
+# Use version 23.10-3.2.2.0 and tgz download option
+wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-3.2.2.0/MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu22.04-x86_64.tgz
+tar -xvzf MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu22.04-x86_64.tgz
+cd MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu22.04-x86_64
+sudo ./mlnxofedinstall
+
