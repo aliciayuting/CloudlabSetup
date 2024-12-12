abf1eb406efdf520a16843edcd46c344f6940f0b
Alicia <Alicia@node1.vortexexpsetup.cascadefuse-pg0.wisc.cloudlab.us>
2024-12-12 17:30:45 -0600

    added nvidia installation script

---
+++ nvidia_driver_setup.sh
@@ -0,0 +1,36 @@
+lpci | grep -i nvidia
+sudo apt install nvidia-utils-550
+
+sudo apt remove --purge '^nvidia-.*'
+sudo apt autoremove
+sudo apt clean
+
+sudo rm -rf /etc/apt/sources.list.d/nvidia-* 
+sudo rm -rf /var/lib/dpkg/info/nvidia* 
+sudo rm -rf /usr/local/cuda*
+
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
+
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
+
+sudo apt update
+
+apt-cache policy nvidia-driver-550
+
+sudo reboot
+
+sudo apt-get --purge remove "*cuda*"
+
+sudo apt-get autoremove
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-4
+
+export PATH=/usr/local/cuda/bin:$PATH
+
+# check via
+nvcc --version
+
