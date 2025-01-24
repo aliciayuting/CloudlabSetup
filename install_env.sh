c9203c76281e825aa22286da46215c37a5d04bc6
Alicia <Alicia@node1.vortexexp.cascadefuse-pg0.wisc.cloudlab.us>
2025-01-24 07:54:50 -0600

    updated env script

---
+++ install_env.sh
@@ -0,0 +1,15 @@
+OPT_HOME=/users/[YOUR-CLOUDLAB-USER-NAME]/opt-dev
+export DERECHO_INSTALL_PREFIX=${OPT_HOME}/
+export CASCADE_INSTALL_PREFIX=${OPT_HOME}/
+export FAISS_INSTALL_PREFIX=${OPT_HOME}/
+export VORTEX_INSTALL_PREFIX=${OPT_HOME}/
+export CURL_INSTALL_PREFIX=${OPT_HOME}/
+export CMAKE_PREFIX_PATH=${OPT_HOME}:/usr/local:${CMAKE_PREFIX_PATH}
+export C_INCLUDE_PATH=${OPT_HOME}/include:/usr/local/include:${C_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH=${OPT_HOME}/include:/usr/local/include:${CPLUS_INCLUDE_PATH}
+
+# Configure library paths
+export LIBRARY_PATH=${OPT_HOME}/lib:/usr/local/lib:${LIBRARY_PATH}
+export LD_LIBRARY_PATH=${OPT_HOME}/lib:/usr/local/lib:${LD_LIBRARY_PATH}
+export PATH=/usr/local/cuda/bin:$PATH
+
