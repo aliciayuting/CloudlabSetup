538cf3a106cc1c5e46376a57db3ecc4e18caf688
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-02-16 12:03:23 -0500

    Update mellanox_driver_setup.sh

--- mellanox_driver_setup.sh
+++ mellanox_driver_setup.sh
@@ -2,5 +2,5 @@
 wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-3.2.2.0/MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu22.04-x86_64.tgz
 tar -xvzf MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu22.04-x86_64.tgz
 cd MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu22.04-x86_64
-sudo ./mlnxofedinstall
+sudo ./mlnxofedinstall -y
 
