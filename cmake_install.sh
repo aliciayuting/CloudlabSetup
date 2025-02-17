d7114ea94aedfe02e8b70dc7e745cbf12bbb2711
aliciayuting <yy354@cornell.edu>
2025-02-17 17:34:28 -0500

    put cmake and mellanox install steps to script

---
+++ cmake_install.sh
@@ -0,0 +1,14 @@
+# install ssl
+
+sudo apt update
+sudo apt install libssl-dev
+
+wget https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0.tar.gz
+tar -xvzf cmake-3.31.0.tar.gz
+cd cmake-3.31.0
+./bootstrap
+make -j32
+sudo make install
+
+
+
