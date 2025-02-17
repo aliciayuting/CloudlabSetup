0f30184a96eb6dbdc01d707de651a6b83c9fe867
aliciayuting <yy354@cornell.edu>
2025-02-17 17:58:43 -0500

    added env setup for cloudlab

--- cloudlab_env.sh
+++ cloudlab_env.sh
@@ -1,4 +1,18 @@
-OPT_HOME=/users/[YOUR-CLOUDLAB-USER-NAME]/opt-dev
+#!/bin/bash
+
+# Check if the OPT_HOME argument is provided
+if [ -z "$1" ]; then
+    echo "your USER_HOME directory not provided. Please provide it as the first argument."
+    echo "you can run this script as $./cloudlab_env_setup.sh /users/[yournetid] or $./cloudlab_env_setup.sh ~"
+    exit 1
+fi
+
+# Set OPT_HOME to the first argument
+USER_HOME=$(echo "$1" | sed 's:/*$::')
+OPT_HOME=${USER_HOME}/opt-dev
+
+# OPT_HOME=/users/[YOUR-CLOUDLAB-USER-NAME]/opt-dev
+
 export DERECHO_INSTALL_PREFIX=${OPT_HOME}/
 export CASCADE_INSTALL_PREFIX=${OPT_HOME}/
 export FAISS_INSTALL_PREFIX=${OPT_HOME}/
