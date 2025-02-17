9446c90e6e2d9a158c9f328af9dd53f94403b8b4
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-02-17 18:08:36 -0500

    Update cloudlab_env.sh

--- cloudlab_env.sh
+++ cloudlab_env.sh
@@ -14,18 +14,23 @@ USER_HOME=$(echo "$1" | sed 's:/*$::')
 OPT_HOME=${USER_HOME}/opt-dev
 
 # OPT_HOME=/users/[YOUR-CLOUDLAB-USER-NAME]/opt-dev
-
+echo "setting up derecho cascade vortex installation env variables"
 export DERECHO_INSTALL_PREFIX=${OPT_HOME}/
 export CASCADE_INSTALL_PREFIX=${OPT_HOME}/
 export FAISS_INSTALL_PREFIX=${OPT_HOME}/
 export VORTEX_INSTALL_PREFIX=${OPT_HOME}/
 export CURL_INSTALL_PREFIX=${OPT_HOME}/
+
+echo "setting up cmake cpp include path env variables"
 export CMAKE_PREFIX_PATH=${OPT_HOME}:/usr/local:${CMAKE_PREFIX_PATH}
 export C_INCLUDE_PATH=${OPT_HOME}/include:/usr/local/include:${C_INCLUDE_PATH}
 export CPLUS_INCLUDE_PATH=${OPT_HOME}/include:/usr/local/include:${CPLUS_INCLUDE_PATH}
 
 # Configure library paths
+echo "setting up library path env variables"
 export LIBRARY_PATH=${OPT_HOME}/lib:/usr/local/lib:${LIBRARY_PATH}
 export LD_LIBRARY_PATH=${OPT_HOME}/lib:/usr/local/lib:${LD_LIBRARY_PATH}
+
+echo "setting up cuda path"
 export PATH=/usr/local/cuda/bin:$PATH
 
