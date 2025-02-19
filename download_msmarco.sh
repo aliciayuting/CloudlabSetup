17c7e2927daed3a54ac92038622c19bbaa36a5e8
Alicia <Alicia@node0.vortexexp.cascadefuse-pg0.wisc.cloudlab.us>
2025-02-18 21:39:00 -0600

    added download msmarco script

---
+++ download_msmarco.sh
@@ -0,0 +1,13 @@
+cd /mydata/
+sudo mkdir msmarco
+cd msmarco
+sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/msmarco_3_clusters.tar.gz
+sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/passages_embeddings_baai1.5small.tsv
+sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/queries_eval_embeddings_baai1.5small.tsv
+
+echo "Finished wget"
+sudo tar -xvzf msmarco_3_clusters.tar.gz
+sudo rm msmarco_3_clusters.tar.gz
+
+echo "extracted msmarco_3_clusters"
+
