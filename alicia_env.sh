894a07bf12442187371d24c27f518ebb24933a1f
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-02-18 23:35:45 -0500

    Update alicia_env.sh

--- alicia_env.sh
+++ alicia_env.sh
@@ -1,9 +1,9 @@
 OPT_HOME=/users/Alicia/opt-dev
 export DERECHO_INSTALL_PREFIX=${OPT_HOME}/
 export CASCADE_INSTALL_PREFIX=${OPT_HOME}/
-export FAISS_INSTALL_PREFIX=${OPT_HOME}/
+export FAISS_INSTALL_PREFIX=/usr/local
 export VORTEX_INSTALL_PREFIX=${OPT_HOME}/
-export CURL_INSTALL_PREFIX=${OPT_HOME}/
+export CURL_INSTALL_PREFIX=/usr/local
 export CMAKE_PREFIX_PATH=${OPT_HOME}:/usr/local:${CMAKE_PREFIX_PATH}
 export C_INCLUDE_PATH=${OPT_HOME}/include:/usr/local/include:${C_INCLUDE_PATH}
 export CPLUS_INCLUDE_PATH=${OPT_HOME}/include:/usr/local/include:${CPLUS_INCLUDE_PATH}
