5a47339724b1275ba5ad2bb8bf9fd299dda62a27
TC Yuan <62821320+tcyuan373@users.noreply.github.com>
2025-03-06 22:15:19 -0500

    Update download_EVQA.sh

--- download_EVQA.sh
+++ download_EVQA.sh
@@ -24,8 +24,8 @@ sudo rm EVQA_test_split.tar.gz EVQA_train_split.tar.gz
 echo "Finished index root"
 
 
-sudo mkdir EVQA_data && sudo unzip EVQA_data.zip -d EVQA_data
-sudo mkdir EVQA_passages && sudo unzip EVQA_passages.zip -d EVQA_passages
+sudo unzip EVQA_data.zip
+sudo unzip EVQA_passages.zip
 sudo mkdir models && sudo unzip models_pipeline1.zip -d models
 sudo mv models/models_pipeline1/*pt models/ && sudo rm models_pipeline1.zip && sudo rm -rf models/models_pipeline1/
 echo "Finished Datasets and model ckpts"
