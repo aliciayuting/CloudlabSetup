6f5cdeb689402e5c76f1638b5d61c38719b3593f
aliciayuting <yy354@cornell.edu>
2025-03-10 12:10:37 +0000

    updated node keys

--- ssh_node.sh
+++ ssh_node.sh
@@ -4,40 +4,43 @@ echo "Need to update this script with the latest cloudlab reservation"
 
 case "$1" in
   node0)
-    ssh Alicia@d7525-10s10337.wisc.cloudlab.us
-    ;;
-  node1)
-    ssh Alicia@d7525-10s10323.wisc.cloudlab.us
-    ;;
-  node2)
     ssh Alicia@d7525-10s10311.wisc.cloudlab.us
     ;;
-  node3)
+  node1)
+    ssh Alicia@d7525-10s10325.wisc.cloudlab.us
+    ;;
+  node2)
     ssh Alicia@d7525-10s10339.wisc.cloudlab.us
     ;;
-  node4)
-    ssh Alicia@d7525-10s10327.wisc.cloudlab.us
-    ;;
-  node5)
-    ssh Alicia@d7525-10s10317.wisc.cloudlab.us
-    ;;
-  node6)
+  node3)
     ssh Alicia@d7525-10s10331.wisc.cloudlab.us
     ;;
-  node7)
-    ssh Alicia@d7525-10s10315.wisc.cloudlab.us
+  node4)
+    ssh Alicia@d7525-10s10319.wisc.cloudlab.us
     ;;
-  node8)
-    ssh Alicia@d7525-10s10309.wisc.cloudlab.us
-    ;;
-  node9)
+  node5)
     ssh Alicia@d7525-10s10321.wisc.cloudlab.us
     ;;
-  node10)
+  node6)
+    ssh Alicia@d7525-10s10317.wisc.cloudlab.us
+    ;;
+  node7)
     ssh Alicia@d7525-10s10335.wisc.cloudlab.us
     ;;
+  node8)
+    ssh Alicia@d7525-10s10333.wisc.cloudlab.us
+    ;;
+  node9)
+    ssh Alicia@d7525-10s10315.wisc.cloudlab.us
+    ;;
+  node10)
+    ssh Alicia@d7525-10s10337.wisc.cloudlab.us
+    ;;
   node11)
-    ssh Alicia@d7525-10s10325.wisc.cloudlab.us
+    ssh Alicia@d7525-10s10327.wisc.cloudlab.us
+    ;;
+  node12)
+    ssh Alicia@d7525-10s10329.wisc.cloudlab.us
     ;;
   *)
     echo "Usage: $0 {node0|node1|...}"
