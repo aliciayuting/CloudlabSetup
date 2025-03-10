157cbbcfe2b5c98702639bea5cd49ce662063c16
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-03-10 13:12:44 -0400

    Update download_EVQA.sh

--- data/download_EVQA.sh
+++ data/download_EVQA.sh
@@ -4,34 +4,38 @@ cd /mydata/
 sudo mkdir EVQA
 cd EVQA
 
-sudo wget https://vortexstorage7348269.blob.core.windows.net/flmrmodels/models_pipeline1.zip
 sudo wget https://vortexstorage7348269.blob.core.windows.net/flmrdata/EVQA_data.zip
 sudo wget https://vortexstorage7348269.blob.core.windows.net/flmrdata/EVQA_passages.zip
 sudo wget https://vortexstorage7348269.blob.core.windows.net/flmrdata/EVQA_test_split.tar.gz
 sudo wget https://vortexstorage7348269.blob.core.windows.net/flmrdata/EVQA_train_split.tar.gz
+
+sudo unzip EVQA_data.zip
+rm -rf EVQA_data.zip
+sudo unzip EVQA_passages.zip
+rm -rf EVQA_passages.zip
+
+echo "Finished EVQA data setup"
+
 sudo wget https://huggingface.co/datasets/BByrneLab/M2KR_Images/resolve/main/EVQA/google-landmark.tar
 sudo wget https://huggingface.co/datasets/BByrneLab/M2KR_Images/resolve/main/EVQA/inat.zip
-echo "Finished wget"
+
 
 sudo mkdir google-landmark 
 sudo tar -xvf google-landmark.tar -C google-landmark
 sudo mkdir inat 
 sudo unzip inat.zip -d inat/
-echo "Finished image root"
+echo "Finished image data installation"
+
 
 sudo mkdir index
 sudo tar -xvzf EVQA_test_split.tar.gz -C index/
 sudo tar -xvzf EVQA_train_split.tar.gz -C index/
 sudo rm EVQA_test_split.tar.gz EVQA_train_split.tar.gz
-echo "Finished index root"
+echo "Finished index installation"
 
 
-sudo unzip EVQA_data.zip
-sudo unzip EVQA_passages.zip
-sudo mkdir models 
-sudo unzip models_pipeline1.zip -d models
-sudo mv models/models_pipeline1/*pt models/
+sudo wget https://vortexstorage7348269.blob.core.windows.net/flmrmodels/models_pipeline1.zip
+sudo unzip -j models_pipeline1.zip -d models
 sudo rm models_pipeline1.zip 
-sudo rm -rf models/models_pipeline1/
-echo "Finished Datasets and model ckpts"
+echo "Finished model ckpts"
 
