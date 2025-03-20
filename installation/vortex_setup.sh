05f51cb464ae2df38a36a6899db02d097831c97b
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-03-20 12:31:46 -0400

    Update vortex_setup.sh

--- installation/vortex_setup.sh
+++ installation/vortex_setup.sh
@@ -6,7 +6,7 @@ cd workspace
 git clone https://github.com/Derecho-Project/vortex.git
 cd vortex
 cd scripts/
-sudo bash install-json.sh
+sudo bash install-curl.sh
 sudo bash install-hnswlib.sh
 
 echo "Installed vortex dependencies"
