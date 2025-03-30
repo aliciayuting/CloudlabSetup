606cbe410a6eeb5153848b2368a04612d47a7a02
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-03-29 22:16:17 -0400

    Update vortex_setup.sh

--- installation/vortex_setup.sh
+++ installation/vortex_setup.sh
@@ -3,13 +3,16 @@ CURR_DIR=$(pwd)
 cd ~
 mkdir workspace
 cd workspace
-git clone https://github.com/Derecho-Project/vortex.git
+git clone https://github.com/aliciayuting/vortex.git
 cd vortex
 cd scripts/
 sudo bash install-curl.sh
 sudo bash install-hnswlib.sh
 
 echo "Installed vortex dependencies"
+
+git checkout ml_pipeline2
+git submodule update --init --recursive
 
 # Return to the original directory
 cd "$CURR_DIR"
