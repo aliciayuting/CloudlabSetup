faa141a5d9439531bedf107c24a2f72b77333ce9
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-04-09 16:12:26 -0400

    Update pipeline_models.sh

--- installation/pipeline_models.sh
+++ installation/pipeline_models.sh
@@ -4,6 +4,9 @@ CURR_DIR=$(pwd)
 
 sudo pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
+# model required by TTS
+sudo pip install nemo_toolkit['all'] 
+
 sudo pip install -r requirements.txt
 
 echo "finished pip installation"
