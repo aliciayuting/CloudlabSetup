67e5f53a1e42bf878f6a290fc5b379c341c48ec4
Alicia Yang <32250648+aliciayuting@users.noreply.github.com>
2025-04-13 12:54:22 -0400

    Update download_msmarco.sh

--- data/download_msmarco.sh
+++ data/download_msmarco.sh
@@ -2,8 +2,8 @@ cd /mydata/
 sudo mkdir msmarco
 cd msmarco
 sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/msmarco_3_clusters.tar.gz
-sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/passages_embeddings_baai1.5small.tsv
-sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/queries_eval_embeddings_baai1.5small.tsv
+#sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/passages_embeddings_baai1.5small.tsv
+#sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/queries_eval_embeddings_baai1.5small.tsv
 sudo wget https://vortexstorage7348269.blob.core.windows.net/msmarco/msmarco_pq.index
 sudo wget https://vortexstorage7348269.blob.core.windows.net/1mmsmarco/queries_audio5000.pkl
 sudo wget https://vortexstorage7348269.blob.core.windows.net/1mmsmarco/ms_macro_1m_queries_embeds.npy
